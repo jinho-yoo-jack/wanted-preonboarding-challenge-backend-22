474f5d978cdbfdd7288c35ead4f6af21aa30d5ba
kimD0ngjun <chickentasty0112@gmail.com>
2024-11-13 18:31:56 +0900

    memo: 레포지토리 역할군 분류

--- src/main/java/com/wanted/clone/oneport/payments/application/port/out/repository/PaymentRepository.java
+++ src/main/java/com/wanted/clone/oneport/payments/application/port/out/repository/PaymentRepository.java
@@ -1,4 +1,8 @@
 package com.wanted.clone.oneport.payments.application.port.out.repository;
 
+/**
+ * 포트 : 시스템 간의 경계 지정
+ * 외부 데이터베이스와 상호작용을 위한 인터페이스
+ */
 public interface PaymentRepository {
 }
--- src/main/java/com/wanted/clone/oneport/payments/representation/port/in/PaymentRequestUseCase.java
+++ src/main/java/com/wanted/clone/oneport/payments/representation/port/in/PaymentRequestUseCase.java
@@ -2,6 +2,10 @@ package com.wanted.clone.oneport.payments.representation.port.in;
 
 import org.springframework.ui.Model;
 
+/**
+ * 포트 : 시스템 간의 경계 지정
+ * 외부 요청이 앱 내부로 들어올 때 사용할 인터페이스
+ */
 public interface PaymentRequestUseCase {
     String renderPgUi();
 }
